1cfda46126bbf015fdf8c5b9be57bc63b342f0e3
xjeyceex <jcmiguel.beltran@gmail.com>
2025-03-24 16:39:59 +0800

    ticket status enhancement

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -59,6 +59,8 @@ const DashboardPage = () => {
       return tickets.filter(
         (ticket) =>
           ticket.ticket_status === "FOR CANVASS" ||
+          ticket.ticket_status === "IN REVIEW" ||
+          ticket.ticket_status === "FOR REVIEW OF SUBMISSIONS" ||
           ticket.ticket_status === "WORK IN PROGRESS"
       ).length;
     }
