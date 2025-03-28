76f23ed38fddadd0c2e1c43ec1778af43c6abf15
xjeyceex <jcmiguel.beltran@gmail.com>
2025-03-28 10:36:59 +0800

    time notification fix

--- components/NotificationMenu.tsx
+++ components/NotificationMenu.tsx
@@ -34,11 +34,11 @@ const NotificationMenu = () => {
   const { notifications, setNotifications } = useNotificationStore();
 
   const unreadCount = notifications.filter(
-    (notif) => !notif.notification_read,
+    (notif) => !notif.notification_read
   ).length;
 
   const getRelativeTime = (timestamp: string) => {
-    return moment(timestamp).fromNow();
+    return moment(timestamp).subtract(8, "hours").fromNow();
   };
 
   const handleNotificationClick = async (notifications: NotificationType) => {
@@ -77,7 +77,7 @@ const NotificationMenu = () => {
       }
 
       const unreadNotifications = res.data?.filter(
-        (notification) => !notification.notification_read,
+        (notification) => !notification.notification_read
       );
       setNotifications(unreadNotifications as NotificationType[]);
     };
@@ -126,8 +126,8 @@ const NotificationMenu = () => {
                   prev.map((notification) =>
                     notification.notification_id === payload.new.notification_id
                       ? { ...notification, ...payload.new }
-                      : notification,
-                  ),
+                      : notification
+                  )
                 );
                 break;
 
@@ -137,12 +137,12 @@ const NotificationMenu = () => {
                   prev.filter(
                     (notification) =>
                       notification.notification_id !==
-                      payload.old.notification_id,
-                  ),
+                      payload.old.notification_id
+                  )
                 );
                 break;
             }
-          },
+          }
         )
         .subscribe();
 
