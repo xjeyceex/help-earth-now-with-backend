5d030896b71c9469d868b4bc13993930cb42ecb0
moralesjromard <omarmoralesjr18@gmail.com>
2025-04-02 10:24:25 +0800

    git pull

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -61,7 +61,7 @@ const DashboardPage = () => {
 
   function countUserTicketsByStatus(
     tickets: DashboardTicketType[],
-    statusType: "OPEN" | "COMPLETED"
+    statusType: "OPEN" | "COMPLETED",
   ) {
     if (statusType === "OPEN") {
       return tickets.filter(
@@ -70,7 +70,7 @@ const DashboardPage = () => {
           ticket.ticket_status === "FOR APPROVAL" ||
           ticket.ticket_status === "FOR REVIEW OF SUBMISSIONS" ||
           ticket.ticket_status === "WORK IN PROGRESS" ||
-          ticket.ticket_status === "FOR REVISION"
+          ticket.ticket_status === "FOR REVISION",
       ).length;
     }
 
@@ -85,7 +85,7 @@ const DashboardPage = () => {
     try {
       setLoading(true);
       const data = await getDashboardTickets(
-        isAdmin ? undefined : user?.user_id
+        isAdmin ? undefined : user?.user_id,
       );
       setTickets(data ?? []);
     } catch (error) {
@@ -346,7 +346,7 @@ const DashboardPage = () => {
                 .sort(
                   (a, b) =>
                     new Date(b.ticket_date_created).getTime() -
-                    new Date(a.ticket_date_created).getTime()
+                    new Date(a.ticket_date_created).getTime(),
                 )
                 .slice(0, 5)
                 .map((ticket, index) => (
